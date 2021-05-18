a9990f4c889dfaa0b8264a63f4704b036046a57d
yonghee555 <yonghee5733@gmail.com>
2021-05-18 21:46:36 +0900

    채팅 입력 시 채팅 메시지 사라지게끔 변경

--- Assets/Scripts/DatabaseAPI.cs
+++ Assets/Scripts/DatabaseAPI.cs
@@ -5,6 +5,7 @@ using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
 using Firebase.Extensions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
@@ -15,6 +16,7 @@ public class DatabaseAPI : MonoBehaviour
     public string key;
     public static string roommaster;
     public ToggleGroup toggleGoup;
+    public TMP_InputField textIF;
 
     private void Awake()
     {
@@ -65,7 +67,8 @@ public class DatabaseAPI : MonoBehaviour
 
     public void PostMessage(Message message, Action callback, Action<AggregateException> fallback)
     {
-        
+        textIF.Select();
+        textIF.text = "";
         var messageJSON = StringSerializationAPI.Serialize(typeof(Message), message); //Message 타입을 JSON으로 파싱
         string messageKey = reference.Child("messages").Child(key).Push().Key;
         reference.Child("messages").Child(key).Child(messageKey).SetRawJsonValueAsync(messageJSON).ContinueWith(task =>
