b5505c183d092df73cb884fa91d23f6f52083146
lmw0122 <74301875+lmw0122@users.noreply.github.com>
2021-06-05 18:29:23 +0900

    리소스 다운

---
+++ Assets/ButtonAndIcon/script/touchTotext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class touchTotext : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private int LevelNext;
+	private bool stop = false;
+
+	void Start() { 
+	LevelNext = PlayerPrefs.GetInt("LevelNum") + 0;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+
+		stop = true;
+			if (LevelNext <= 7 && stop == true)
+			{
+				if (PlayerPrefs.HasKey("LevelNum"))
+				{
+					LevelNext += 1;
+					PlayerPrefs.SetInt("LevelNum", LevelNext);
+					PlayerPrefs.Save();
+					Debug.Log("next");
+					Application.LoadLevel(LevelNext);
+					stop = false;
+				}
+			}
+			if (LevelNext > 7) {
+				PlayerPrefs.SetInt("LevelNum",0);
+				PlayerPrefs.Save();
+				Application.LoadLevel(0);
+			}
+
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		
+	}
+}
